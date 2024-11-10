3e624b7aeb37b59c257883b1232314bea93be3c9
hieu453 <hieult0405@gmail.com>
2024-11-10 13:44:10 +0700

    update v2 show correct answer

--- resources/views/home/question/correct-answers.blade.php
+++ resources/views/home/question/correct-answers.blade.php
@@ -92,73 +92,75 @@
             @foreach ($questions as $key => $question)
                 <h2>{{ $question->title }}</h2>
                 @foreach ($question->options as $option)
-                    @foreach ($userAnswers as $answer)
-                        @if (count($userAnswers) == count($questions))
-                            @if ($answer->id == $option->id && $answer->is_correct && $answer->question->id == $question->id)
-                                <div class="form-check" style="background-color: aqua;">
-                                    <label class="form-check-label">
-                                        {{ $option->text }}
-                                    </label>
-                                </div>
-                            @elseif ($option->is_correct && $answer->question->id == $question->id)
-                                <div class="form-check" style="background-color: green;">
-                                    <label class="form-check-label">
-                                        {{ $option->text }}
-                                    </label>
-                                </div>
-                            @elseif ($answer->id == $option->id && !$answer->is_correct && $answer->question->id == $question->id)
-                                <div class="form-check" style="background-color: red;">
-                                    <label class="form-check-label">
-                                        {{ $option->text }}
-                                    </label>
-                                </div>
-                            @elseif ($answer->question->id == $question->id && !$option->is_correct)
-                                <div class="form-check">
-                                    <label class="form-check-label">
-                                        {{ $option->text }}
-                                    </label>
-                                </div>
-                            {{-- @else
-                                <div class="form-check {{ $option->is_correct ? 'bg-success' : '' }}">
-                                    <label class="form-check-label">
-                                        {{ $option->text }}
-                                    </label>
-                                </div> --}}
-                            @endif
-                        @else
-                            @if ($answer->id == $option->id && $answer->is_correct && $answer->question->id == $question->id)
-                                <div class="form-check" style="background-color: aqua;">
-                                    <label class="form-check-label">
-                                        {{ $option->text }}
-                                    </label>
-                                </div>
-                            @elseif ($option->is_correct && $answer->question->id == $question->id)
-                                <div class="form-check" style="background-color: green;">
-                                    <label class="form-check-label">
-                                        {{ $option->text }}
-                                    </label>
-                                </div>
-                            @elseif ($answer->id == $option->id && !$answer->is_correct && $answer->question->id == $question->id)
-                                <div class="form-check" style="background-color: red;">
-                                    <label class="form-check-label">
-                                        {{ $option->text }}
-                                    </label>
-                                </div>
-                            @elseif ($answer->question->id == $question->id && !$option->is_correct)
-                                <div class="form-check">
-                                    <label class="form-check-label">
-                                        {{ $option->text }}
-                                    </label>
-                                </div>
+                    @if ($userAnswers)
+                        @foreach ($userAnswers as $answer)
+                            @if (count($userAnswers) == count($questions))
+                                @if ($answer->id == $option->id && $answer->is_correct && $answer->question->id == $question->id)
+                                    <div class="form-check" style="background-color: aqua;">
+                                        <label class="form-check-label">
+                                            {{ $option->text }}
+                                        </label>
+                                    </div>
+                                @elseif ($option->is_correct && $answer->question->id == $question->id)
+                                    <div class="form-check" style="background-color: green;">
+                                        <label class="form-check-label">
+                                            {{ $option->text }}
+                                        </label>
+                                    </div>
+                                @elseif ($answer->id == $option->id && !$answer->is_correct && $answer->question->id == $question->id)
+                                    <div class="form-check" style="background-color: red;">
+                                        <label class="form-check-label">
+                                            {{ $option->text }}
+                                        </label>
+                                    </div>
+                                @elseif ($answer->question->id == $question->id && !$option->is_correct)
+                                    <div class="form-check">
+                                        <label class="form-check-label">
+                                            {{ $option->text }}
+                                        </label>
+                                    </div>
+                                @endif
                             @else
-                                <div class="form-check {{ $option->is_correct ? 'bg-success' : '' }}">
-                                    <label class="form-check-label">
-                                        {{ $option->text }}
-                                    </label>
-                                </div>
+                                @if ($answer->id == $option->id && $answer->is_correct && $answer->question->id == $question->id)
+                                    <div class="form-check" style="background-color: aqua;">
+                                        <label class="form-check-label">
+                                            {{ $option->text }}
+                                        </label>
+                                    </div>
+                                @elseif ($option->is_correct && $answer->question->id == $question->id)
+                                    <div class="form-check" style="background-color: green;">
+                                        <label class="form-check-label">
+                                            {{ $option->text }}
+                                        </label>
+                                    </div>
+                                @elseif ($answer->id == $option->id && !$answer->is_correct && $answer->question->id == $question->id)
+                                    <div class="form-check" style="background-color: red;">
+                                        <label class="form-check-label">
+                                            {{ $option->text }}
+                                        </label>
+                                    </div>
+                                @elseif ($answer->question->id == $question->id && !$option->is_correct)
+                                    <div class="form-check">
+                                        <label class="form-check-label">
+                                            {{ $option->text }}
+                                        </label>
+                                    </div>
+                                @else
+                                    <div class="form-check {{ $option->is_correct ? 'bg-success' : '' }}">
+                                        <label class="form-check-label">
+                                            {{ $option->text }}
+                                        </label>
+                                    </div>
+                                @endif
                             @endif
-                        @endif
-                    @endforeach
+                        @endforeach
+                    @else
+                        <div class="form-check {{ $option->is_correct ? 'bg-success' : '' }}">
+                            <label class="form-check-label">
+                                {{ $option->text }}
+                            </label>
+                        </div>
+                    @endif
                 @endforeach
             @endforeach
         </div>
